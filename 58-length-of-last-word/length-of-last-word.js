703c3b2f8ea0ff356b18e5a2163468fe9aa2b936
fna1788-stack <fna1788@gmail.com>
2026-08-04 12:30:03 +0530

    Time: 0 ms (100.00%) | Memory: 54.9 MB (8.28%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+    let a=s.trim()
+    a=a.split(" ")
+    for (i=a.length-1;i<=a.length-1;i++){
+        b=a[i].length
+         
+    }
+            return b
+
+};
